ba13dc2e6e9cdff24b9eb3933da61940e5643458
Ricardo Queiroz Ribeiro <ricardorqr@gmail.com>
2019-01-28 23:51:58 -0500

    Secure service lyer inplemented

--- src/main/java/com/uxpsystems/assignement/config/SecurityConfig.java
+++ src/main/java/com/uxpsystems/assignement/config/SecurityConfig.java
@@ -1,21 +1,54 @@
 package com.uxpsystems.assignement.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.anyRequest()
-			.permitAll()
-			.and()
-			.csrf()
-			.disable();
-	}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//			.anyRequest()
+//			.permitAll()
+//			.and()
+//			.csrf()
+//			.disable();
+//	}
+	@Autowired
+    private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
+ 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+          .withUser("admin").password(passwordEncoder().encode("admin"))
+          .authorities("ROLE_USER");
+    }
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+          .antMatchers("/api/**").permitAll()
+          .anyRequest().authenticated()
+          .and()
+          .httpBasic()
+          .authenticationEntryPoint(authenticationEntryPoint);
+ 
+        http.addFilterAfter(new CustomFilter(),
+          BasicAuthenticationFilter.class);
+    }
+ 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
